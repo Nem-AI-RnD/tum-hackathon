ac36a4ae46b3108864b758d5465f2cc4ca8f7285
Alex V <alex.platten22@gmail.com>
2025-11-21 23:19:40 +0100

    clean up

--- src/ai_eval/services/file.py
+++ src/ai_eval/services/file.py
@@ -14,78 +14,78 @@ from ai_eval.config import global_config as glob
 from ai_eval.services.blueprint_file import BaseService
 from ai_eval.services.logger import LoggerFactory
 
-my_logger = LoggerFactory(handler_type="Stream", verbose=True).create_module_logger()
+my_logger = LoggerFactory().create_module_logger()
 
 
-class CSVService(BaseService):
-    def __init__(
-        self,
-        path: str = "",
-        root_path: str = glob.DATA_PKG_DIR,
-        delimiter: str = "\t",
-        encoding: str = "UTF-8",
-        schema_map: Optional[dict] = None,
-        verbose: bool = False,
-    ):
-        """Generic read/write service for CSV files
-        Args:
-            path (str, optional): Filename. Defaults to "".
-            delimiter (str, optional): see pd.read_csv. Defaults to "\t".
-            encoding (str, optional): see pd.read_csv. Defaults to "UTF-8".
-            schema_map (Optional[dict], optional): mapping scheme for renaming of columns, see pandas rename. Defaults to None.
-            root_path (str, optional): root path where file is located. Defaults to glob.UC_DATA_DIR.
-            verbose (bool, optional): should user information be displayed? Defaults to False.
-        """
-        super().__init__(path, root_path)
-        self.path = os.path.join(root_path, path)
-        # Ensure the directory exists
-        if not os.path.exists(root_path):
-            os.makedirs(root_path, exist_ok=True)
-        self.delimiter = delimiter
-        self.verbose = verbose
-        self.encoding = encoding
-        self.schema_map = schema_map
+# class CSVService(BaseService):
+#     def __init__(
+#         self,
+#         path: str = "",
+#         root_path: str = glob.DATA_PKG_DIR,
+#         delimiter: str = "\t",
+#         encoding: str = "UTF-8",
+#         schema_map: Optional[dict] = None,
+#         verbose: bool = False,
+#     ):
+#         """Generic read/write service for CSV files
+#         Args:
+#             path (str, optional): Filename. Defaults to "".
+#             delimiter (str, optional): see pd.read_csv. Defaults to "\t".
+#             encoding (str, optional): see pd.read_csv. Defaults to "UTF-8".
+#             schema_map (Optional[dict], optional): mapping scheme for renaming of columns, see pandas rename. Defaults to None.
+#             root_path (str, optional): root path where file is located. Defaults to glob.UC_DATA_DIR.
+#             verbose (bool, optional): should user information be displayed? Defaults to False.
+#         """
+#         super().__init__(path, root_path)
+#         self.path = os.path.join(root_path, path)
+#         # Ensure the directory exists
+#         if not os.path.exists(root_path):
+#             os.makedirs(root_path, exist_ok=True)
+#         self.delimiter = delimiter
+#         self.verbose = verbose
+#         self.encoding = encoding
+#         self.schema_map = schema_map
 
-    def doRead(self, **kwargs: Any) -> pd.DataFrame:
-        """Read data from CSV
-        Args:
-            **kwargs (Dict[str, Any]): Additional arguments for pandas read_csv.
-        Returns:
-            pd.DataFrame: data converted to dataframe
-        """
-        try:
-            df = pd.read_csv(
-                filepath_or_buffer=self.path,
-                encoding=self.encoding,
-                delimiter=self.delimiter,
-                **kwargs,
-            )
-            if self.verbose:
-                my_logger.info(f"CSV Service read from file: {str(self.path)}")
-            if self.schema_map:
-                df.rename(columns=self.schema_map, inplace=True)
-            return df
-        except Exception as e:
-            my_logger.error(f"Error reading CSV file {self.path}: {e}")
-            return pd.DataFrame()
+#     def doRead(self, **kwargs: Any) -> pd.DataFrame:
+#         """Read data from CSV
+#         Args:
+#             **kwargs (Dict[str, Any]): Additional arguments for pandas read_csv.
+#         Returns:
+#             pd.DataFrame: data converted to dataframe
+#         """
+#         try:
+#             df = pd.read_csv(
+#                 filepath_or_buffer=self.path,
+#                 encoding=self.encoding,
+#                 delimiter=self.delimiter,
+#                 **kwargs,
+#             )
+#             if self.verbose:
+#                 my_logger.info(f"CSV Service read from file: {str(self.path)}")
+#             if self.schema_map:
+#                 df.rename(columns=self.schema_map, inplace=True)
+#             return df
+#         except Exception as e:
+#             my_logger.error(f"Error reading CSV file {self.path}: {e}")
+#             return pd.DataFrame()
 
-    def doWrite(self, X: pd.DataFrame, **kwargs: Any) -> None:
-        """Write X to CSV file
-        Args:
-            X (pd.DataFrame): input data
-        """
-        try:
-            X.to_csv(
-                path_or_buf=self.path,
-                encoding=self.encoding,
-                sep=self.delimiter,
-                index=False,
-                **kwargs,
-            )
-            if self.verbose:
-                my_logger.info(f"CSV Service output to file: {str(self.path)}")
-        except Exception as e:
-            my_logger.error(f"Error writing CSV file {self.path}: {e}")
+#     def doWrite(self, X: pd.DataFrame, **kwargs: Any) -> None:
+#         """Write X to CSV file
+#         Args:
+#             X (pd.DataFrame): input data
+#         """
+#         try:
+#             X.to_csv(
+#                 path_or_buf=self.path,
+#                 encoding=self.encoding,
+#                 sep=self.delimiter,
+#                 index=False,
+#                 **kwargs,
+#             )
+#             if self.verbose:
+#                 my_logger.info(f"CSV Service output to file: {str(self.path)}")
+#         except Exception as e:
+#             my_logger.error(f"Error writing CSV file {self.path}: {e}")
 
 
 class YAMLService(BaseService):
@@ -139,58 +139,6 @@ class YAMLService(BaseService):
                 my_logger.error(exc)
 
 
-class TXTService(BaseService):
-    def __init__(
-        self,
-        path: str = "",
-        root_path: str = glob.DATA_PKG_DIR,
-        verbose: bool = True,
-    ):
-        """
-        Generic read/write service for TXT files.
-
-        Args:
-            path (Optional[str]): Filename. Defaults to "".
-            root_path (str): Root path where file is located. Defaults to glob.DATA_DIR.
-            verbose (bool): Should user information be displayed? Defaults to True.
-        """
-        super().__init__(path, root_path)
-        self.path = os.path.join(root_path, path)
-        self.verbose = verbose
-
-    def doRead(self, **kwargs: Any) -> List[str]:
-        """
-        Read data from TXT file.
-
-        Returns:
-            List[str]: Input data.
-        """
-        try:
-            with open(self.path, **kwargs) as f:
-                df = f.read().splitlines()
-            if self.verbose:
-                my_logger.info(f"TXT Service read from file: {str(self.path)}")
-        except Exception as e0:
-            my_logger.error(e0)
-            df = []
-        return df
-
-    def doWrite(self, X: List[str], **kwargs: Any) -> None:
-        """
-        Write data to TXT file.
-
-        Args:
-            X (List[str]): Input data.
-        """
-        try:
-            with open(self.path, "w", **kwargs) as f:
-                f.write("\n".join(X))
-            if self.verbose:
-                my_logger.info(f"TXT Service output to file: {str(self.path)}")
-        except Exception as e0:
-            my_logger.error(e0)
-
-
 class JSONService(BaseService):
     def __init__(
         self, path: str = "", root_path: str = glob.DATA_PKG_DIR, verbose: bool = True
@@ -238,59 +186,6 @@ class JSONService(BaseService):
                 json.dump(X, outfile, ensure_ascii=False, indent=4, **kwargs)
                 if self.verbose:
                     my_logger.info(f"JSON Service Output to File: {self.path}")
-            except Exception as exc:
-                my_logger.error(exc)
-
-
-class TOMLService(BaseService):
-    def __init__(
-        self,
-        path: str = "",
-        root_path: str = glob.DATA_PKG_DIR,
-        verbose: bool = False,
-    ):
-        """
-        Generic read/write service for TOML files.
-
-        Args:
-            path (Optional[str]): Filename. Defaults to "".
-            root_path (str): Root path where file is located. Defaults to glob.CODE_DIR.
-            verbose (bool): Should user information be displayed? Defaults to False.
-        """
-        super().__init__(path, root_path)
-        self.root_path = root_path
-        self.path = os.path.join(root_path, path)
-        self.verbose = verbose
-
-    def doRead(self, **kwargs: Any) -> Dict:
-        """
-        Read data from TOML file.
-
-        Returns:
-            Dict: Imported TOML file.
-        """
-        with open(os.path.join(self.root_path, self.path), "r") as stream:
-            try:
-                toml_load = toml.load(stream, **kwargs)
-                if self.verbose:
-                    my_logger.info(f"Read: {self.root_path + (self.path or '')}")
-            except Exception as exc:
-                my_logger.error(exc)
-                return {}
-        return toml_load
-
-    def doWrite(self, X: Dict, **kwargs: Any) -> None:
-        """
-        Write dictionary to TOML file.
-
-        Args:
-            X (Dict): Input dictionary.
-        """
-        with open(os.path.join(self.root_path, self.path), "w") as outfile:
-            try:
-                toml.dump(X, outfile)
-                if self.verbose:
-                    my_logger.info(f"Write to: {self.root_path + (self.path or '')}")
             except Exception as exc:
                 my_logger.error(exc)
 
